6a3de04a6e992cfc1c9a9e9de2316fc7b3443b3b
Juicechuan <cwang24@brandeis.edu>
2012-12-05 21:13:30 -0500

    version 0.0 for SNLP PA4

--- cs134-SNLP_PA/PA4/main.py
+++ cs134-SNLP_PA/PA4/main.py
@@ -1,27 +1,48 @@
+# -*- coding: utf-8 -*-
 #import nltk
 #import numpy as np
 #from maxent import MaxEnt
 #from helper import Instance
 #import util
 #import evaluator
+from TransitionParsing import TranSys,leftArc,rightArc,shift
+from maxent import MaxEnt
+
 
 def load_sentence(fname):
     f = open(fname,'r')
-    data_instance = []
+    raw_data = []
     sentence = [] 
     for line in f:
-        if not line.strip():
+        if line.strip():
             line = line.split()
             word_index = line[0]
             token = line[1]
             POS = line[3]
             head_index = line[6]
             label = line[7]
-            element = tuple(word_index,token,POS,head_index,label)
+            element = tuple((int(word_index),token,POS,int(head_index),label))
             sentence.append(element)
         else:
-            data_instance.append(sentence)
+            raw_data.append(sentence)
+            #print sentence
             sentence=[]
             
-    return data_instance
-            
+    return raw_data
+
+#--------------------------
+sentence_instances = load_sentence("data/test")
+transition_codebook = {'LeftArc':leftArc,
+                       'RightArc':rightArc,
+                       'Shift':shift}
+
+tranSys = TranSys(transition_codebook)
+instance_list = []
+for sentence in sentence_instances:
+    CT_pairs = tranSys.Gold_parse(sentence)
+    print [c.sigma.list for c,t in CT_pairs]
+    instances = tranSys.feature_extract(CT_pairs)
+    instance_list.append(instances)
+
+ME = MaxEnt()
+ME.train(instance_list)
